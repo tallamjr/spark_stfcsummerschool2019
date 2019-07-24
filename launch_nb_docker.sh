3189ef4cac39120254cc40749e6168caa779d2a4
Julien <peloton@lal.in2p3.fr>
2019-07-25 01:46:55 +0200

    Increase dedicated resources

--- launch_nb_docker.sh
+++ launch_nb_docker.sh
@@ -7,4 +7,5 @@ SPARKFITS="com.github.astrolabsoftware:spark-fits_2.11:0.8.3"
 # Run jupyter through the Docker
 docker run -it --rm  \
 	-v $PWD:/home/jovyan/work:rw -p 8888:8888 -p 400:4040 \
-	spark_stfc /usr/local/spark/bin/pyspark --master local[*] --packages $SPARKFITS
+	spark_stfc /usr/local/spark/bin/pyspark --master local[*] \
+    --driver-memory 2g --executor-memory 8g --packages $SPARKFITS
